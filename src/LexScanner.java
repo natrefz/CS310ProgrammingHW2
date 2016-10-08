dd1340484add6a276547c29394028a69ce2329ee
Jared Bartrug <jerad4@gmail.com>
2016-10-08 18:10:29 -0400

    little bit of scanner work

--- src/LexScanner.java
+++ src/LexScanner.java
@@ -1,10 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class LexScanner {
 
-	final private String[] keywords = {"endif","then","else","if",")", "(", "!=","=","/","*","-","+","end","begin","procedue"};
+	final private String[] keywords = {"endif","then","else","if","end","begin","procedue", "integer", "variable",")", "(", "!=","=","/","*","-","+"};
 	private String current;
 	private Scanner s;
 	public LexScanner(File f) throws FileNotFoundException {
@@ -13,7 +14,70 @@ public class LexScanner {
 	}
 	public int lex(){
 		char c = s.next(".").charAt(0);
-		return 0;
+		if(c==' '||c=='\n'){
+			lex();
+		}
+		else if(c=='('){
+			current = ""+c;
+			return Arrays.asList(keywords).indexOf('(');
+		}
+		else if(c==')'){
+			current = ""+c;
+			return Arrays.asList(keywords).indexOf(')');
+		}
+		else if(c=='='){
+			current = ""+c;
+			return Arrays.asList(keywords).indexOf('=');
+		}
+		else if(c=='!'){
+			if(s.next(".").charAt(0)=='='){
+				current = "!=";
+				return Arrays.asList(keywords).indexOf("!=");
+			}
+		}
+		else if(c=='/'){
+			current = ""+c;
+			return Arrays.asList(keywords).indexOf('/');
+		}
+		else if(c=='+'){
+			current = ""+c;
+			return Arrays.asList(keywords).indexOf('+');
+		}
+		else if(c=='-'){
+			current = ""+c;
+			return Arrays.asList(keywords).indexOf('-');
+		}
+		else if(c=='*'){
+			current = ""+c;
+			return Arrays.asList(keywords).indexOf('*');
+		}
+		else if(c==';'){
+			current = ""+c;
+			return Arrays.asList(keywords).indexOf(';');
+		}
+		else if(c==':'){
+			if(s.next(".").charAt(0)=='='){	
+				current = ":=";
+				return Arrays.asList(keywords).indexOf(":=");
+			}
+		}
+		else if(current.isEmpty() && Character.isDigit(c)){
+			while(Character.isDigit(c)){
+				current = current +c;
+				c = s.next(".").charAt(0);
+			}
+			return Arrays.asList(keywords).indexOf("integer");
+		}
+		else if(Character.isAlphabetic(c)){
+			current = current+ c;
+			for(int i =0;i<7;i++){
+				if(keywords[i].equals(current)){
+					return i;
+				}
+			}
+			lex();
+		}
+		return -1;
 	}
 	public String getCurrentToken() {
 		return current;
